e3012b9a817189ec688d8931fc9575affb2d0ed0
Angel Orellana <angel@paperculture.com>
2022-08-19 07:33:11 -0500

    first models and views

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
+$(document).ready(function (){
+    const todoItems = new TodoItems([
+        new TodoItem({ description: "TODO ITEM 1" }),
+        new TodoItem({ description: "TODO ITEM 2" })
+    ]);
+
+    const todoItemsView = new TodoItemsView({ model: todoItems });
+
+    $("body").append(todoItemsView.render().$el);
+})
---
+++ js/todoItem.js
@@ -0,0 +1,7 @@
+const TodoItem = Backbone.Model.extend({
+    validate: function (attrs) {
+        if(!attrs.description){
+            return "Description is required";
+        }
+    }
+});
